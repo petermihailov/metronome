abac4f279c5e9ed7b42d30bf670030842e183b5c
pmihailov <usernameispeter@gmail.com>
2023-09-24 18:31:35 +0400

    fix: version file

--- src/components/Version/Version.tsx
+++ src/components/Version/Version.tsx
@@ -1,14 +1,20 @@
 import { memo, useEffect, useState } from 'react';
 
+import env from '../../env';
+
 import classes from './Version.module.css';
 
 const Version = () => {
   const [version, setVersion] = useState('');
 
   useEffect(() => {
-    fetch('/public/version')
-      .then((res) => res.text())
-      .then((v) => setVersion('v' + v.trim()));
+    if (!env.DEV) {
+      fetch('/version')
+        .then((res) => res.text())
+        .then((v) => setVersion('v' + v.trim()));
+    } else {
+      setVersion('v' + APP_VERSION);
+    }
   }, []);
 
   return <div className={classes.root}>{version}</div>;
