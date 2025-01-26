dbe0c1279f924c748351c87a3ad98c3b5bdaa114
pmihailov <usernameispeter@gmail.com>
2025-01-26 15:09:22 +0400

    feat: timer position

--- src/components/blocks/TodayTimer/TodayTimer.tsx
+++ src/components/blocks/TodayTimer/TodayTimer.tsx
@@ -1,14 +1,21 @@
+import clsx from 'clsx'
 import { memo } from 'react'
 
+import { useMetronomeStore } from '../../../store/useMetronomeStore'
 import { usePlayingTimeStore } from '../../../store/usePlayingTimeStore'
 import { timeFormat } from '../../../utils/format'
 
 import classes from './TodayTimer.module.css'
 
 const TodayTimer = () => {
+  const { isTraining } = useMetronomeStore(({ isTraining }) => ({ isTraining }))
   const { dayTime } = usePlayingTimeStore(({ time }) => ({ dayTime: timeFormat(time.day) }))
 
-  return <time className={classes.todayTimer}>Total today: {dayTime}</time>
+  return (
+    <time className={clsx(classes.todayTimer, { [classes.center]: isTraining })}>
+      Total today: {dayTime}
+    </time>
+  )
 }
 
 export default memo(TodayTimer)
